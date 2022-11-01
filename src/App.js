5d2ea74dac6633a0995e6bd7063f25b75d247fee
unknown <anougok@gmail.com>
2022-11-01 02:28:22 +0100

    make it better

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {Footer,Blog,Features,Header,Possibility,Whatgpt3} from './containers'
+import {Cta,Brand,Navbar} from './components'
+
+const  App=() => {
+  return (
+    <div className='App'>
+        <div className="gradient__bg">
+          <Navbar/>
+          <Header/>
+        </div>
+        <Brand/>
+        <Whatgpt3/>
+        <Features/>
+        <Possibility/>
+        <Cta/>
+        <Blog/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
